6240bfa6d444f66324ab3f162e83ec1dd098b314
Ademitidayo <adenijibimmy08@gmail.com>
2024-03-13 01:07:33 -0700

    first commit

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,2 @@
+-- Creates the database hbtn_0c_0 in my MySQL server if not exit.
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
---
+++ 0x0D-SQL_introduction/README.md
@@ -0,0 +1,3 @@
+0x0D-SQL_introduction
+learning the neeeded command codes to start using sql
+
