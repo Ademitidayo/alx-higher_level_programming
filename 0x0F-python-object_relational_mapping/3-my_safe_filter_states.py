f288cef78dc17f6840fa0851ee9169c8dc37d578
ademitidayo <adenijibimm08@gmail.com>
2024-06-28 21:34:33 -0700

    task

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,13 +1,55 @@
 #!/usr/bin/python3
+"""
+Script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
+"""
+
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (sys.argv[4],))
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+    # Command line arguments: username, password, database name, state name searched
+    if len(sys.argv) != 5:
+        print("Usage: {} username password database state_name".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to MySQL server
+    try:
+        db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+        )
+    except MySQLdb.Error as e:
+        print("Error connecting to MySQL database:", e)
+        sys.exit(1)
+
+    # Create a cursor object using cursor() method
+    cursor = db.cursor()
+
+    # Use placeholder %s for the state_name to prevent SQL injection
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id"
+
+    try:
+        # Execute SQL query with state_name as parameter
+        cursor.execute(query, (state_name,))
+
+        # Fetch all the rows and print them
+        results = cursor.fetchall()
+        for row in results:
+            print(row)
+
+    except MySQLdb.Error as e:
+        print("Error executing MySQL query:", e)
+        sys.exit(1)
+
+    finally:
+        # Close cursor and database connection
+        cursor.close()
+        db.close()
