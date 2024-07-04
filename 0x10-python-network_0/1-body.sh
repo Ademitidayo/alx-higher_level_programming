337c4b48f9c94022b2dd511c847fcac9d89944f1
ademitidayo <adenijibimm08@gmail.com>
2024-07-04 16:03:36 -0700

    using curl to request and get response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a GET request to the URL, and displays the response body
-curl -s -L "${1}"
+[ "$(curl -s -o /tmp/response_body -w "%{http_code}" "$1")" -eq 200 ] && cat /tmp/response_body; rm /tmp/response_body
